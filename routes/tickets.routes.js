d29b97a1cfbd134369a4db19ecc381e03b49d1d7
Архипцев Богдан <60853092+bog-danius@users.noreply.github.com>
2026-09-21 00:26:22 +0300

    Update tickets.routes.js

--- routes/tickets.routes.js
+++ routes/tickets.routes.js
@@ -2,7 +2,8 @@ import express from "express";
 import { prisma } from "../prisma/prisma.js";
 
 const router = express.Router();
-
+const express = require('express');
+const router = express.Router();
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 };
